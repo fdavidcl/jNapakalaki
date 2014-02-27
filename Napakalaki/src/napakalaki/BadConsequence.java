c0ae65511ba876681df71265522f220e473476b8
NCordon <nacho.cordon.castillo@gmail.com>
2014-02-27 12:59:41 +0100

    Añadida clase Monster

--- Napakalaki/src/napakalaki/BadConsequence.java
+++ Napakalaki/src/napakalaki/BadConsequence.java
@@ -6,6 +6,7 @@
 
 package napakalaki;
 
+import java.util.ArrayList;
 /**
  *
  * @author fdavidcl
@@ -17,11 +18,21 @@ public class BadConsequence {
     private int nHiddenTreasures;
     private boolean death;
     
+    private ArrayList<TreasureKind> specificHiddenTreasures;
+    private ArrayList<TreasureKind> specificVisibleTreasures;
+    
     BadConsequence(String text, int levels, int nVisible, int nHidden) {
         this.text = text;
         this.levels = levels;
         nVisibleTreasures = nVisible;
         nHiddenTreasures = nHidden;
+    }
+    BadConsequence(String text, int levels, ArrayList<TreasureKind> tVisible, 
+            ArrayList<TreasureKind> tHidden){
+        this.text = text;
+        this.levels = levels;
+        specificVisibleTreasures = tVisible;
+        specificHiddenTreasures = tHidden;
     }
     
     BadConsequence(String text, boolean death) {
@@ -47,5 +58,11 @@ public class BadConsequence {
     
     public boolean getDeath() {
         return death;
+    }
+    public String toString(){
+        return "Mal rollo: " + text + "\nNiveles: " + Integer.toString(levels)
+                + "\nTesoros visibles: " + Integer.toString(nVisibleTreasures)
+                + "\nTesoros no visibles: " + Integer.toString(nHiddenTreasures)
+                + "\nMuerte: " + (death ? "Sí": "No");
     }
 }
