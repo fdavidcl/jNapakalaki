e09b5fa6bd8539202755abe71fee81377488c3be
David Charte <fdavidcl@outlook.com>
2014-03-20 12:51:44 +0100

    Treasure terminado

--- Napakalaki/src/Game/Treasure.java
+++ Napakalaki/src/Game/Treasure.java
@@ -11,5 +11,37 @@ package Game;
  * @author fdavidcl
  */
 class Treasure {
+    private String name;
+    private int goldCoins;
+    private int minBonus;
+    private int maxBonus;
+    private TreasureKind type;
     
+    public Treasure(String n, int g, int min, int max, TreasureKind t) {
+        name = n;
+        goldCoins = g;
+        minBonus = min;
+        maxBonus = max;
+        type = t;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public int getGoldCoins() {
+        return goldCoins;
+    }
+
+    public int getMinBonus() {
+        return minBonus;
+    }
+
+    public int getMaxBonus() {
+        return maxBonus;
+    }
+
+    public TreasureKind getType() {
+        return type;
+    }
 }
