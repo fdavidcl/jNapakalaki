757a5758f49ad32e84d1c1f9b5d8bb5a44d68006
NCordon <nacho.cordon.castillo@gmail.com>
2014-03-20 13:04:32 +0100

    CombatResult creado

---
+++ Napakalaki/src/Game/CombatResult.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Game;
+
+/**
+ * @author Francisco David Charte Luque
+ * @author Ignacio Cordón Castillo
+ */
+public enum CombatResult {
+    WINANDWINGAME,
+    WIN,
+    LOSE,
+    LOSEANDESCAPE,
+    LOSEANDDIE
+}
