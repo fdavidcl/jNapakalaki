57917fb70a1b1fb7261e6ad6256e830008337db5
NCordon <nacho.cordon.castillo@gmail.com>
2014-05-12 17:12:22 +0200

    línea--

--- Napakalaki/src/Game/Napakalaki.java
+++ Napakalaki/src/Game/Napakalaki.java
@@ -93,7 +93,6 @@ public class Napakalaki {
         return currentPlayer.getHiddenTreasures();
     }
 
-    @SuppressWarnings("empty-statement")
     public boolean nextTurn() {
         boolean stateOK = nextTurnAllowed();
 
