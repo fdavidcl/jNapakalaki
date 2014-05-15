f4439146b2ace096ae9b9e0dd6d49515e9c26362
David Charte <fdavidcl@outlook.com>
2014-05-15 11:59:40 +0200

    Clases nuevas!

---
+++ Napakalaki/src/Game/CultistPlayer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Game;
+
+/**
+ *
+ * @author fdavidcl
+ */
+public class CultistPlayer extends Player {
+
+    public CultistPlayer(String name, Cultist c) {
+        super(name);
+    }
+    
+}
