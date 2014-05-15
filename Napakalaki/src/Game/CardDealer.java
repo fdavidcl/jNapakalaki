cd82be2e61f5e35f1d4aafe7ce8b8e33c374f9e7
David Charte <fdavidcl@outlook.com>
2014-05-15 13:15:13 +0200

    Mazo de sectarios

--- Napakalaki/src/Game/CardDealer.java
+++ Napakalaki/src/Game/CardDealer.java
@@ -172,6 +172,12 @@ public class CardDealer {
     
     private void initCultistCardDeck() {
         // Añadir cartas sectarias
+        unusedCultists.add(new Cultist("Sectario", 1));
+        unusedCultists.add(new Cultist("Sectario", 2));
+        unusedCultists.add(new Cultist("Sectario", 1));
+        unusedCultists.add(new Cultist("Sectario", 2));
+        unusedCultists.add(new Cultist("Sectario", 1));
+        unusedCultists.add(new Cultist("Sectario", 1));
     }
 
     private void shuffleTreasures() {
